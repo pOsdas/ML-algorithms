06d1f20ff67f9390b6f99bc91d09bd7769f77299
pOsdas <ratmir1099@mail.ru>
2025-03-21 19:03:34 +0300

    add KNN using sklearn

---
+++ KNN/from_sklearn.py
@@ -0,0 +1,15 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+data = load_iris()
+X, y = data.data, data.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(X_train, y_train)
+y_pred = knn.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print("Точность модели:", accuracy)
